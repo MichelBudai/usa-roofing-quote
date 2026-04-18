34cfdd26f4ba6ae7a26573ccdad28a48fb404496
Stephane <decostefax@gmail.com>
2026-04-18 08:44:56 +0200

    fix: update roofing phone to MarketCall (844) 212-9167

--- config/sites/roofing.ts
+++ config/sites/roofing.ts
@@ -4,8 +4,8 @@ export const roofingConfig = {
   slug: "roofing",
   siteUrl: "https://usa-roofing-quote.com",
   siteName: "USA Roofing Quote",
-  phoneTel: "tel:+18335675933",
-  phoneDisplay: "(833) 567-5933",
+  phoneTel: "tel:+18442129167",
+  phoneDisplay: "(844) 212-9167",
   ga4Id: "G-R5XHFLY3R3", // ← à remplir après création GA4
   services: [
     { slug: "roof-repair",          label: "Roof Repair Quote" },
